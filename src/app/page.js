9e3c689afd0e4cf5e55c37c838dbf760c7676624
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-08-17 14:27:39 +0100

    page styling

--- src/app/page.js
+++ src/app/page.js
@@ -95,55 +95,77 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex-1 flex flex-col bg-slate-800 text-gray-300">
       {/* HEADER */}
-      <section></section>
-      {clickThrough ? (
-        <section section className="text-black w-[200px]">
-          <div className="bg-gray-200 p-2">
-            {questionsAndAnswers[currentQuestion].question}
+      <section className="bg-black flex justify-between items-center bg-slate-900">
+        <div className="hover:bg-gray-400 rounded p-2">
+          <img src="./logo.png" alt="logo" className="h-12 p-2"></img>
+        </div>
+        <h3 className="hover:bg-gray-400 rounded p-2">Jobs Tracker</h3>
+      </section>
+      {/* MAIN BODY */}
+      <section
+        className="bg-cover pb-80 h-[800px]"
+        style={{ backgroundImage: "url('/background.png')" }}
+      >
+        {clickThrough ? (
+          <section className="w-screen flex justify-center items-center h-full">
+            <section className="text-black w-[350px] mt-16 bg-slate-700 text-white rounded">
+              <div className=" p-2">
+                {questionsAndAnswers[currentQuestion].question}
+              </div>
+              <div className="p-2">
+                {questionsAndAnswers[currentQuestion].answers.map(
+                  (answer, index) => (
+                    <div
+                      key={index}
+                      className="p-1 bg-white text-slate-900 m-2 rounded-lg border-2 hover:bg-slate-500"
+                      onClick={() =>
+                        handleAnswer(
+                          questionsAndAnswers[currentQuestion].question,
+                          answer
+                        )
+                      }
+                    >
+                      {answer}
+                    </div>
+                  )
+                )}
+              </div>
+            </section>
+          </section>
+        ) : (
+          <section className="flex flex-col items-center text-center h-full">
+            <h1 className="text-[54px] mt-16">
+              POWERING FUTURES:
+              <br></br>
+              NEXT STEPS
+            </h1>
+            <div className="text-[25px] mt-16">
+              WHAT DO YOU WANT TO DO NEXT?
+            </div>
+            <div
+              className="mt-8 bg-orange-400 hover:bg-orange-700 rounded p-2"
+              onClick={() => handleClickThrough()}
+            >
+              Try our online quiz.
+            </div>
+          </section>
+        )}
+        {currentQuestion == questionsAndAnswers.length - 1 ? (
+          <div className="w-full flex flex-col items-center">
+            <button
+              className="bg-orange-400 hover:bg-orange-700 text-white p-2 rounded"
+              onClick={handleClick}
+            >
+              Submit Answers
+            </button>
+            {displayData && <>{data}</>}
           </div>
-          <div className="bg-gray-200 p-2">
-            {questionsAndAnswers[currentQuestion].answers.map(
-              (answer, index) => (
-                <div
-                  key={index}
-                  className="p-1 border-black border-2"
-                  onClick={() =>
-                    handleAnswer(
-                      questionsAndAnswers[currentQuestion].question,
-                      answer
-                    )
-                  }
-                >
-                  {answer}
-                </div>
-              )
-            )}
-          </div>
-        </section>
-      ) : (
-        <section className="flex flex-col items-center text-center">
-          <h1 className="text-[84px]">
-            POWERING FUTURES:
-            <br></br>
-            NEXT STEPS
-          </h1>
-          <div className="text-[40px] mt-16">WHAT DO YOU WANT TO DO NEXT?</div>
-          <div className="mt-8" onClick={() => handleClickThrough()}>
-            Try our online quiz.
-          </div>
-        </section>
-      )}
-
-      {currentQuestion == questionsAndAnswers.length - 1 ? (
-        <>
-          <button onClick={handleClick}>Submit Answers</button>
-          {displayData && <>{data}</>}
-        </>
-      ) : (
-        <></>
-      )}
+        ) : (
+          <></>
+        )}
+      </section>
     </main>
   );
 }
